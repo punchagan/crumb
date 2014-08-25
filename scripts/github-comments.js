9234c648d588639b3dd229070c8e6a2f12ca17af
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-25 16:48:18 -0400

    First cut of the work-flow.

---
+++ scripts/github-comments.js
@@ -0,0 +1,111 @@
+define(["jquery", "octokit", "oauth"], function($, Octokit, OAuth) {
+
+    self = this;
+
+    /* OAuth doesn't work as I expect, with require... */
+    OAuth = window.OAuth;
+
+    this.insertLoginButton = function(){
+        $('<button>').text('Post Comment').appendTo($('#github_thread'))
+            .click(
+                function(evt){
+                    var button = evt.target;
+                    OAuth.initialize('WWANKMvsCbVkU_8CmQphyrfhKew');
+                    OAuth.popup('github', {})
+                        .done(function(result){
+                            console.log(result);
+                            self.github = new Octokit({
+                                token: result.access_token
+                            });
+                            self.repo = self.github.getRepo.apply(self.github, self.getRepoDetails())
+                            $(button).replaceWith(self.insertCommentForm())
+                        })
+                        .fail(function(){
+                            $(button).replaceWith(self.insertCommentForm(true))
+                        })
+                });
+    }
+
+    this.getRepoDetails = function() {
+        return [$('#github_thread').attr('data-repo-user'), $('#github_thread').attr('data-repo-name')]
+    }
+
+    this.insertCommentForm = function(failed) {
+        failed = failed || false;
+        if (!failed) {
+            var form = $('<form>');
+            $('<textarea id="new-comment">').appendTo(form);
+            $('<input type="submit">').appendTo(form).click(self.commentOnIssue);
+        } else {
+            var form = $("<span>Failed to Login. Cannot comment.</span>");
+        }
+        return form;
+    }
+
+    this.commentOnIssue = function(evt) {
+        evt.preventDefault();
+        var text = $('#new-comment').val();
+        var issue = $('#github_thread').attr('data-github-issue-number') || 0;
+
+        if (!issue) {
+            // Create a new issue.
+            self.repo.createIssue(location.pathname)
+                .done(function(issue_data){
+                    issue = issue_data.number;
+                    $('#github_thread').attr('data-github-issue-number', issue);
+                    self.postComment(issue, text);
+                })
+                .fail(function(data){
+                    alert('Failed to create a new issue for this page.');
+                });
+        } else {
+            self.postComment(issue, text);
+        }
+    }
+
+    this.postComment = function(issue, text) {
+        // Post new comment.
+        self.repo.createComment(issue, text)
+            .done(function(comment){
+                var comments_ul = $('#comment-list');
+                if (comments_ul.length == 0) {
+                    self.insertIssueComments([comment]);
+                } else {
+                    $('<li>').text(comment.body).appendTo(comments_ul);
+                }
+                $('#new-comment').val('');
+            })
+            .fail(function(){alert('failed to post comment')});
+    }
+
+    this.insertIssueComments = function(comments) {
+        var comments_ul = $("<ul id='comment-list'>");
+        $('#github_thread').append(comments_ul);
+        comments.forEach(function(comment){
+            $('<li>').text(comment.body).appendTo(comments_ul);
+        });
+    }
+
+    this.getIssueByTitle = function(issues){
+        var filtered_issues = issues.filter(function(issue){
+            return issue.title.replace(/^\s+|\s+$/g, '') == location.pathname;
+        });
+
+        if (filtered_issues.length == 0){
+            issues.nextPage && issues.nextPage().then(self.getIssueByTitle);
+        } else {
+            var issue = filtered_issues[0];
+            $('#github_thread').attr('data-github-issue-number', issue.number);
+            self.repo.getComments(issue.number).then(self.insertIssueComments);
+        }
+
+    }
+
+    $(document).ready(function(){
+        self.github = new Octokit({});
+        self.repo = self.github.getRepo.apply(self.github, self.getRepoDetails());
+        self.repo.getIssues("all").then(self.getIssueByTitle);
+        self.insertLoginButton();
+    });
+
+});
---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// For any third party dependencies, like jQuery, place them in the lib folder.
+
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'scripts',
+    paths: {
+        jquery: "//code.jquery.com/jquery-1.11.0.min",
+        octokit: "https://rawgit.com/punchagan/github/83e4c38e930f0353905ed557ed7ac531c49a96ae/octokit",
+        oauth: "https://rawgit.com/oauth-io/oauth-js/d3a5cc38418e8e78d637e4072712b25360476663/dist/oauth.min"
+    }
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['github-comments']);
